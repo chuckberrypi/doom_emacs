5d33ec1d26ad9a7b0ac884cdbda0f38ece2dfeba
David Stearns <d.f.stearns@gmail.com>
2022-08-05 16:29:42 -0700

    ifixed config

--- config.el
+++ config.el
@@ -2,10 +2,3 @@
 
 (org-babel-load-file "~/.doom.d/doom-config.org")
 
-(with-current-buffer "Hamilton_NOP.org"
-  (org-element-map (org-element-parse-buffer) 'headline
-    (lambda (el)
-      (org-element-property :contents-begin el))))
-
-(with-current-buffer "Hamilton_NOP.org"
-  )

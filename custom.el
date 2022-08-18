cc44ff598106071c3de889c2bdef3ac67cd0b7e0
David Stearns <d.f.stearns@gmail.com>
2022-08-17 18:38:22 -0700

    throw away

--- custom.el
+++ custom.el
@@ -5,6 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
    '("~/work_org/WH_PHarbor.org" "/Users/davidstearns/org/Projects.org" "/Users/davidstearns/org/gods_shadow.org" "/Users/davidstearns/org/journal.org" "/Users/davidstearns/org/notes.org" "/Users/davidstearns/org/raspberrypi_notes.org" "/Users/davidstearns/org/reviews.org" "/Users/davidstearns/org/test-temp.org" "/Users/davidstearns/org/todo.org" "/Users/davidstearns/org/transcludetest.org"))
+ '(vimish-fold-blank-fold-header "Folded")
+ '(vimish-fold-persist-on-saving t)
+ '(vimish-fold-show-lines t)
  '(warning-suppress-types '((clojure-mode-hook))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -13,3 +16,4 @@
  ;; If there is more than one, they won't work right.
  )
  
+(put 'customize-group 'disabled nil)

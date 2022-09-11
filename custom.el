d0d6ddd5c342c3e6a468a449518f53435ca216f7
chuckberrypi <d.f.stearns@gmail.com>
2022-09-11 11:47:51 -0700

    progress on protocol

--- custom.el
+++ custom.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   '("~/work_org/WH_PHarbor.org" "/Users/davidstearns/org/Projects.org" "/Users/davidstearns/org/gods_shadow.org" "/Users/davidstearns/org/journal.org" "/Users/davidstearns/org/notes.org" "/Users/davidstearns/org/raspberrypi_notes.org" "/Users/davidstearns/org/reviews.org" "/Users/davidstearns/org/test-temp.org" "/Users/davidstearns/org/todo.org" "/Users/davidstearns/org/transcludetest.org"))
- '(warning-suppress-types '((clojure-mode-hook))))
+ '(warning-suppress-types '((initialization) (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
